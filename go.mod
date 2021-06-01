6afc6e2253500cb7e1084d1d66392ea6ee92dd7a
Anthony Bui <nhut.anthony.bui@gmail.com>
2021-06-01 17:08:18 -0500

    feat: Hello world completed, first route setup

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/Personal-Library/basic-go-api
+
+go 1.16
+
+require github.com/gorilla/mux v1.8.0
--- main.go
+++ main.go
@@ -2,8 +2,18 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
+func homeLink(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome home!")
+}
+
 func main() {
-	fmt.Println("Hello")
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/", homeLink)
+	log.Fatal(http.ListenAndServe(":8080", router))
 }
